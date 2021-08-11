7b157357b3e8e170e899c8741efe8924232dd421
Christina Nguyen <cnguyen4290@yahoo.com>
2021-08-11 01:08:28 -0500

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(depth){
   // Write your code here!
+  if (depth <=400)
+  {return 'This one is on me!'}
+  else if (depth <=2499)
+  {return 'I will gladly take your thirty bucks.'}
+  else (depth>=2500)
+  {return 'No can do.'}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(place){
   // Write your code here!
+return ternaryCheckCity = place === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip)
+  { case 'generous' : return 'Thank you so much.';
+case 'not as generous' : return 'Thank you.';
+default: return 'Bye.';
+}
+  
 }
